7e4d451d772e481e1d1920cfcad4dadcaaf5c73a
usmanhb123 <bangkarteamwork73@gmail.com>
2022-10-23 23:53:26 +0700

    commit function

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -66,4 +66,67 @@ void main() {
   print({a < b});
   print({a >= b});
   print({a <= b});
+
+  
+  //conditional
+  print("conditional");
+  var nilai;
+  nilai = 80;
+
+  if (nilai >= 80) {
+    print("A");
+  } else if (nilai <= 80 && nilai >= 50) {
+    print("B");
+  } else {
+    print("tidak lulus");
+  }
+
+  print("..........");
+  nilai >= 80 ? print("A") : print("tidak A");
+
+  //function
+  print("function");
+
+  hitungnilai();
+  hitungnilai1(75, 90);
+  var p = hitungnilai1(50, 2, 56);
+  print(p);
+  var n = hitungnilai2(mapel1: 50, mapel2: 2);
+  print(n);
+  hitungnilai3(79, 100);
+  var o = hitungnilai3(79, 100);
+}
+
+
+//function
+hitungnilai() {
+  print("hitung nilai");
+}
+
+//positional argument
+hitungnilai1(mapel1, mapel2, [mapel3]) {
+  var nilaiakhir;
+  if (mapel3 != null) {
+    nilaiakhir = mapel1 / mapel2 + mapel3;
+  } else {
+    nilaiakhir = mapel1 / mapel2;
+  }
+  return nilaiakhir;
+}
+
+//name argument
+hitungnilai2({mapel1, mapel2}) {
+  var nilaiakhir;
+  if (mapel2 != null) {
+    nilaiakhir = mapel1 / mapel2;
+  } else {
+    nilaiakhir = mapel1;
+  }
+  return nilaiakhir;
+}
+
+//vold
+hitungnilai3(mapel1, mapel2) {
+  var nilaiakhir = mapel1 + mapel2;
+  print(nilaiakhir);
 }
