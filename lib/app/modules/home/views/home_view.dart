f56c149f21f1b73b9848a280d4479b8cab98b754
usmanhb123 <bangkarteamwork73@gmail.com>
2022-11-12 08:17:24 +0700

    pertemuan 3

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:get/get.dart';
 
@@ -9,16 +10,110 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        backgroundColor: const Color.fromARGB(255, 162, 205, 240),
+        body: Row(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Container(
+                  color: const Color.fromARGB(255, 162, 205, 240),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 100,
+                          alignment: Alignment.topCenter,
+                          child: const Image(
+                            image: AssetImage('assets/icons/icon.png'),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100,
+                          child: Center(
+                            child: InkWell(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 40,
+                                    width: 100,
+                                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(40), color: Colors.white),
+                                    child: const Icon(FontAwesomeIcons.desktop, color: Colors.grey,),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Text("Home", style: TextStyle(color: Colors.grey, fontSize: 16),)
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100,
+                          child: Center(
+                            child: InkWell(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 40,
+                                    width: 100,
+                                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(40), color: Colors.white),
+                                    child: const Icon(FontAwesomeIcons.desktop, color: Colors.grey,),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Text("Home", style: TextStyle(color: Colors.grey, fontSize: 16),)
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100,
+                          child: Center(
+                            child: InkWell(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 40,
+                                    width: 100,
+                                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(40), color: Colors.white),
+                                    child: const Icon(FontAwesomeIcons.desktop, color: Colors.grey,),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Text("Home", style: TextStyle(color: Colors.grey, fontSize: 16),)
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100,
+                          child: Center(
+                            child: InkWell(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 40,
+                                    width: 100,
+                                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(40), color: Colors.white),
+                                    child: const Icon(FontAwesomeIcons.desktop, color: Colors.grey,),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Text("Home", style: TextStyle(color: Colors.grey, fontSize: 16),)
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
+            Expanded(
+                flex: 15,
+                child: Container(
+                  color: const Color.fromARGB(255, 252, 252, 252),
+                ))
+          ],
+        ));
   }
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,12 +3,18 @@ part of 'app_pages.dart';
 
 abstract class Routes {
   Routes._();
+  static const LOGIN = _Paths.LOGIN;
   static const HOME = _Paths.HOME;
   static const PROFILE = _Paths.PROFILE;
+  static const TASK = _Paths.TASK;
+  static const FRIENDS = _Paths.FRIENDS;
 }
 
 abstract class _Paths {
   _Paths._();
+  static const LOGIN = '/login';
   static const HOME = '/home';
   static const PROFILE = '/profile';
+  static const TASK = '/task';
+  static const FRIENDS = '/friends';
 }
