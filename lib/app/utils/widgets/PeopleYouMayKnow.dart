89ac84c60bd726f8a30fb1cf2998aa06d8c538dc
usmanhb123 <bangkarteamwork73@gmail.com>
2023-01-17 19:57:58 +0700

    tugas video 18 task management

--- lib/app/utils/widgets/PeopleYouMayKnow.dart
+++ lib/app/utils/widgets/PeopleYouMayKnow.dart
@@ -1,63 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../data/controllers/authController.dart';
 
 class PeopleYouMayKnow extends StatelessWidget {
- final authCon = Get.find<AuthController>();
+  final authCon = Get.find<AuthController>();
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 200,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        clipBehavior: Clip.antiAlias,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(50),
-                  child: const Image(
-                    image: NetworkImage(
-                        'https://ecs7.tokopedia.net/blog-tokopedia-com/uploads/2021/03/Freepik2.jpg'),
-                  ),
-                ),
-                const Positioned(
-                  bottom: 10,
-                  left: 50,
-                  child: Text(
-                    'Surti',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: SizedBox(
-                    height: 36,
-                    width: 36,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.zero,
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(50),
+      child: FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        future: authCon.getPeople(),
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          var data = snapshot.data!.docs;
+          return ListView.builder(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            clipBehavior: Clip.antiAlias,
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              var hasil = data[index].data();
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image(
+                        image: NetworkImage(hasil['photo']),
+                        height: 200,
+                        width: 220,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      left: 75,
+                      child: Text(
+                        hasil['name'],
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: SizedBox(
+                        height: 36,
+                        width: 36,
+                        child: ElevatedButton(
+                          onPressed: () => authCon.addFriends(hasil['email']),
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          ),
+                          child: Icon(Icons.add_circle_outline),
                         ),
                       ),
-                      child: Icon(Icons.add_circle_outline),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              );
+            },
           );
-        },
+        }),
       ),
     );
   }
